63688e27c636833fc53bb5228849226580ba664d
Vincent Harvey <vharv008@uottawa.ca>
2018-07-11 20:39:31 -0400

    Adding a new program into the DB is completed!!!

---
+++ GymApp/Models/WorkoutViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GymApp.Models
+{
+    public class WorkoutViewModel
+    {
+        public string UserID { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 5)]
+        [Display(Name = "Name")]
+        public string WorkoutName { get; set; }
+        
+        [StringLength(1000, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 0)]
+        [Display(Name = "Description")]
+        public string WorkoutDescription { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Start Date")]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "End Date")]
+        public DateTime EndDate { get; set; }
+    }
+}
